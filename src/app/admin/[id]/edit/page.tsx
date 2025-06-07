bc70c8748d76a4c4ec685a4a3d87d732233db34f
Fehehehe <febryt22@gmail.com>
2025-06-07 14:05:52 +0700

    refactor: adjust some type

--- src/app/admin/[id]/edit/page.tsx
+++ src/app/admin/[id]/edit/page.tsx
@@ -3,7 +3,7 @@ import { apiUrl } from "@/lib/axiosClient";
 import { UserAttribute } from "@/types/app-type";
 import EditAdminForm from "./editAdminForm";
 
-export default async function ViewAdminPage({ params }: { params: { id: string } }) {
+export default async function ViewAdminPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     const response = await fetch(`${apiUrl}admin/account/${id}`, { method: 'GET' })
     let admin = await response.json();
